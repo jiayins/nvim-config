ed7400290b116792b7d8768d389b1e84b4825ea7
SongJiayin <286438365@qq.com>
2022-05-08 21:53:41 +0800

    first commit

---
+++ lua/basic.lua
@@ -0,0 +1,32 @@
+vim.g.encoding = "UTF-8"
+vim.o.fileencoding = 'utf-8'
+-- 显示行号
+vim.wo.number = true
+--高亮所在行
+vim.wo.cursorline = true
+-- 显示左侧图标指示列
+vim.wo.signcolumn = "yes"
+
+vim.o.tabstop = 4
+vim.bo.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftround = true
+
+vim.o.shiftwidth = 4
+vim.bo.shiftwidth = 4
+
+vim.o.expandtab = true
+vim.bo.expandtab = true
+vim.o.autoindent = true
+vim.bo.autoindent = true
+vim.o.smartindent = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- 等待mappings
+vim.o.timeoutlen = 500
+
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.opt.termguicolors = true
---
+++ lua/keybindings.lua
@@ -0,0 +1,64 @@
+vim.g.mapleaer = " "
+vim.g.maplocalleader = " "
+
+local map = vim.api.nvim_set_keymap
+local opt = {noremap = true, silent = true }
+
+map('v', '<', '<gv', opt)
+map('v', '>', '>gv', opt)
+
+map("n", "sv", ":vsp<CR>", opt)
+map("n", "sh", ":sp<CR>", opt)
+map("n", "sc", "<C-w>c", opt)
+map("n", "so", "<C-w>o", opt)
+
+
+map("n", "s>", ":vertical resize +20<CR>", opt)
+map("n", "s<", ":vertical resize -20<CR>", opt)
+map("n", "s=", "<C-w>=", opt)
+map("n", "sj", ":resize +10<CR>",opt)
+map("n", "sk", ":resize -10<CR>",opt)
+
+map("n", "<A-h>", "<C-w>h", opt)
+map("n", "<A-j>", "<C-w>j", opt)
+map("n", "<A-k>", "<C-w>k", opt)
+map("n", "<A-l>", "<C-w>l", opt)
+
+map("i", "jj", "<ESC>", opt)
+
+-- bufferline 左右切换
+map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
+map("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
+
+-- nvimTree
+map('n', '<C-n>', ':NvimTreeToggle<CR>', opt)
+
+local pluginKeys = {}
+
+-- lsp 回调函数快捷键设置
+pluginKeys.maplsp = function(mapbuf)
+  -- rename
+  mapbuf('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opt)
+  -- code action
+  mapbuf('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opt)
+  -- go xx
+  mapbuf('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opt)
+  mapbuf('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opt)
+  mapbuf('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opt)
+  mapbuf('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opt)
+  mapbuf('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opt)
+  -- diagnostic
+  mapbuf('n', 'go', '<cmd>lua vim.diagnostic.open_float()<CR>', opt)
+  mapbuf('n', 'gp', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opt)
+  mapbuf('n', 'gn', '<cmd>lua vim.diagnostic.goto_next()<CR>', opt)
+  -- mapbuf('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opt)
+  -- leader + =
+  mapbuf('n', '<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>', opt)
+  -- mapbuf('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opt)
+  -- mapbuf('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opt)
+  -- mapbuf('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opt)
+  -- mapbuf('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opt)
+  -- mapbuf('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opt)
+end
+
+return pluginKeys
